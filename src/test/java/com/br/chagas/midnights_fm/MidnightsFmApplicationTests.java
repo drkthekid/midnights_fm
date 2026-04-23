ebad2c5f65c8492a1bbb0f04972d8c316b07fcfc
drkthekid <ddchagas.d1@gmail.com>
2026-04-23 17:00:04 -0300

    feat: environment docker

--- src/test/java/com/br/chagas/midnights_fm/MidnightsFmApplicationTests.java
+++ src/test/java/com/br/chagas/midnights_fm/MidnightsFmApplicationTests.java
@@ -2,8 +2,10 @@ package com.br.chagas.midnights_fm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MidnightsFmApplicationTests {
 
 	@Test
